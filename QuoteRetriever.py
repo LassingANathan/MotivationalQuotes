f613a182b04aa343150cc0eb2c6f24d68b5d21de
Nathan <lassinganathan@gmail.com>
2025-02-13 17:30:36 -0800

    add function to retrieve quote from non-favorite list

--- QuoteRetriever.py
+++ QuoteRetriever.py
@@ -10,11 +10,49 @@ class QuoteRetriever():
     # returns: a tuple with the quote as the first element and the author as the second element, or None as the second element if no author was included
     def retrieveRandomQuote(self, includeFavorites=False) -> tuple:
         with open(self.filePath, 'r') as f:
-            if not includeFavorites: # Do not include favorites list
-                # Read file
-                lines = f.readlines()
-            else: # Include favorites list
-                pass
+            # Read file
+            lines = f.readlines()
+            favoriteQuotes = [] # Parallel arrays for storing a quote's CONTENT
+            favoriteQuoteAuthors = [] # Parallel arrays for storing a quote's AUTHOR (or None)
+            
+            # Get all quotes that aren't favorites
+            for line in lines:
+                if line.strip() == "FAVORITES": # We've reached the favorites
+                    if not includeFavorites: # If we don't include favorites, stop reading quotes
+                        break
+                    else: # If we DO include favorites, then go to the next line and proceed to add all the favorites
+                        continue
+                    
+                else:
+                    # Split line to separate author
+                    quoteParts = line.split(self.authorDelimiter)
+                    
+                    # Determine if the quote had an author
+                    if (len(quoteParts) == 1): # No author
+                        favoriteQuotes.append(quoteParts[0].strip())
+                        favoriteQuoteAuthors.append(None)
+                    elif (len(quoteParts) == 2): # Quote had an author
+                        favoriteQuotes.append(quoteParts[0].strip())
+                        favoriteQuoteAuthors.append(quoteParts[1].strip())
+                    else: # A quote likely had an @ symbol in it, and everything is ruined
+                        favoriteQuotes.append(None)
+                        favoriteQuotes.append(None)             
+
+            
+            # Generate a quote until one works
+            while True:
+                randQuoteIndex = random.randint(0, len(favoriteQuotes) - 1)
+                
+                quoteContent = favoriteQuotes[randQuoteIndex]
+                
+                # Pick another quote if this quote's content is None, it means something is messed up with it
+                if quoteContent == None:
+                    continue
+                
+                quoteAuthor = favoriteQuoteAuthors[randQuoteIndex]
+                
+                # Return the tuple described at function header
+                return (quoteContent, quoteAuthor)
     
     # Retrieve a random quote from the favorites list
     # returns: a tuple with the quote as the first element and the author as the second element, or None as the second element if no author was included
